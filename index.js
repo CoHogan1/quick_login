c4936f238691551785104fd5faada80f3737d33e
Conner Hogan <ConnerRHogan@gmail.com>
2021-07-05 01:08:47 -0600

    test commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+require('dotenv').config()
+const PORT = process.env.PORT || 3003
+const app = express()
+
+const session = require('express-session');
+const systemControllers = require('./controllers/server.js')
+const Attempt = require('./models/schema.js')
+const Client = require('./models/users.js')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+const mongoose = require('mongoose')
+const information = require('./models/dbInfo.js')
+const mongoURI = process.env.MONGODBURI
+const db = mongoose.connection
+
+mongoose.connect(mongoURI , { // start connection to db
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},()=>{
+    console.log("Mongo connection is  established.")
+})
+
+// connection error handeling.
+db.on('error', (err)=> console.log(err.message + ' Mongo is not running!!!'))
+db.on('connected', ()=> console.log('Mongo connected: ' + mongoURI))
+db.on('disconnected', ()=> console.log('Mongo is now Disconnected, Have a good day!'))
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false, // here may save after page refresh
+    saveUninitialized: false
+}))
+
+
+// middleware to ensure user is logged in.
+const isAuthenticated = (req, res, next) => {
+    console.log('session auth')
+    if (req.session.currentUser) {
+        // console.log(req.session.currentUser)
+        return next()
+    } else {
+        //console.log(req.sessions.currentUser)
+        res.redirect('/sessions/new')
+    }
+}
+
+const homeControllers = require('./controllers/server')
+app.use('/home', isAuthenticated,  homeControllers)
+//app.use('/home', homeControllers) // make new user
+
+const usersControllers = require('./controllers/users')
+//app.use('/users', isAuthenticated, usersControllers) // use once user is set up
+app.use('/users', usersControllers) // make new user
+
+const sessionsControllers = require('./controllers/sessions')
+app.use('/sessions', sessionsControllers)
+
+
+// HOMEPAGE Route
+app.get('/', (req, res) => {
+    res.render('home.ejs', {currentUser: req.session.currentUser})
+})
+
+app.get('/home', (req, res) => {
+    res.render('home.ejs', {currentUser: req.session.currentUser})
+})
+
+app.listen(PORT, (req, res)=>{
+    console.log('Music App server listening......')
+})
